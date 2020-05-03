ed1f6d6c85da82c54feb1857846f4dd132ed26ae
Rogelio González Rosas <rogelio.glez@gmail.com>
2020-05-03 16:18:32 -0500

    End of Section 3

--- Reactivities.Api/Startup.cs
+++ Reactivities.Api/Startup.cs
@@ -31,6 +31,13 @@ namespace Reactivities.Api
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -41,8 +48,16 @@ namespace Reactivities.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                //The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+                
+            }
 
             // app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
