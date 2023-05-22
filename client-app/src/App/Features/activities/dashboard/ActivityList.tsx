cade634876a16925f091ac8e5f4f79ceae59df8f
Rogelio Gonzalez <rogelio.glez@gmail.com>
2023-05-21 22:10:13 -0400

    End of Section 6

--- client-app/src/App/Features/activities/dashboard/ActivityList.tsx
+++ client-app/src/App/Features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,22 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../Models/activity";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+  const [target, setTarget] = useState("");
+
+  const handleDeleteActivity = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
       <Item.Group divided>
@@ -30,10 +39,12 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                   color="blue"
                 />
                 <Button
-                  onClick={() => deleteActivity(activity.id)}
+                  name={activity.id}
+                  onClick={(e) => handleDeleteActivity(e, activity.id)}
                   floated="right"
                   content="Delete"
                   color="red"
+                  loading={submitting && target === activity.id}
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
--- client-app/src/App/Features/activities/form/ActivityForm.tsx
+++ client-app/src/App/Features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
   closeForm: () => void;
   activity?: Activity;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
-export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit }: Props) {
+export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit, submitting }: Props) {
 
   const initialState = selectedActivity ?? {
     id: "",
@@ -30,7 +31,7 @@ export default function ActivityForm({ closeForm, activity: selectedActivity, cr
   function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value })
-    
+
   }
 
   return (
@@ -39,10 +40,10 @@ export default function ActivityForm({ closeForm, activity: selectedActivity, cr
         <Form.Input placeholder="title" value={activity.title} name="title" onChange={handleInputChange} />
         <Form.TextArea placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
         <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
-        <Form.Input placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
+        <Form.Input type="date" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
         <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange} />
         <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange} />
-        <Button onClick={handleSubmit} floated="right" positive type="submit" content="Submit" />
+        <Button onClick={handleSubmit} floated="right" positive type="submit" content="Submit" loading={submitting} />
         <Button
           onClick={closeForm}
           floated="right"
